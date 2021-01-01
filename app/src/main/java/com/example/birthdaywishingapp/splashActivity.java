7898ee6a798f8e11963d1532e0f6fc8c04e4dd4c
iampranat <praveer.pranat2@gmail.com>
2021-01-01 20:28:38 +0530

    Birthday-Wishing App

---
+++ app/src/main/java/com/example/birthdaywishingapp/splashActivity.java
@@ -0,0 +1,31 @@
+package com.example.birthdaywishingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class splashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        Thread thread= new Thread(){
+            public void run(){
+                try {
+                    sleep(2000);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent=new Intent(splashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+            };thread.start();
+        }
+    }
